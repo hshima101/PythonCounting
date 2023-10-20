8175ea294402a21671d1e5a559e15d8d113a6a9d
hshima101 <67283100+hshima101@users.noreply.github.com>
2023-10-20 16:27:59 -0400

    Add files via upload
    The first working version of the program. Tweaks will be made to clean up the code

--- Count.py
+++ Count.py
@@ -34,4 +34,69 @@ class Testing:
             print(f'Error loading data from {file}: {str(e)}')
             return []
 
+class FileWrite:
+
+    def write(self):
+        file_path = "number.txt"
+
+        with open(file_path, 'w') as file:
+            file.write("Hello World!\n")
+            file.write("This is a sentence")
+    
+    def read(self, file_path):
+        try:
+            with open(file_path, 'r') as file:
+                file_content = file.read()
+                print("File content: ")
+                print(file_content)
+        except FileNotFoundError:
+            print("File not found")
+        except Exception as e:
+            print("An error occured")
+
+        
+class Counting:
+    #everything runs in one function
+    #First the program will read from the .txt file and save the number in a variable
+    #After that the counting function will run until the quit key has been pressed
+    #Finally the program will write the new number to the .txt file
+
+    def write(self,numberInput):
+        file_path = "number.txt"
+
+        with open(file_path, 'w') as file:
+            file.write(numberInput)
+
+    def read(self, file_path):
+        try:
+            with open(file_path, 'r') as file:
+                file_content = file.read()
+                print("File content: ")
+                print(file_content)
+                
+        except FileNotFoundError:
+            print("File not found")
+        except Exception as e:
+            print("An error occured")
+
+        return file_content
+
+
+    def Function(self):
+        index = 0
+
+        while True:
+            print("initializing function")
+            print("Type a letter (or 'q' to quit)", end = ' ', flush = True)
+            char = msvcrt.getch().decode('utf-8')
+
+            if char == 'q':
+                print("Exiting loop")
+                
+                break
+            else:
+                print(f"You typed {char}")
+                index = index + 1
+                print(index)
+        return index
 
--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-from Count import Testing
+from Count import Counting
 
 def main():
     print("Main has been invoked")
-    obj = Testing()
-    #obj.keypress()
-    loaded = obj.load('number.txt')
-    print(loaded)
+
+    b = Counting()
+
+    # Read the content of 'number.txt' and convert it to an integer
+    try:
+        with open('number.txt', 'r') as file:
+            numbers = int(file.read())
+    except (ValueError, FileNotFoundError):
+        print("Error: 'number.txt' does not contain a valid integer.")
+        return
+
+    print("Number from file:", numbers)
+
+    # Perform your calculations on the integer
+    convertedNum = numbers + b.Function()
+    print("Final Number:", convertedNum)
+
+    # Convert the result back to a string and write it to the file
+    final = str(convertedNum)
+    with open('number.txt', 'w') as file:
+        file.write(final)
 
 if __name__ == "__main__":
     main()
